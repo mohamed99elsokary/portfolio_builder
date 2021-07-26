9a99e55f794260210ad4893a0861da56725aea75
mohamed99elsokary <mohamed99elsokary@gmail.com>
2021-07-26 15:38:13 +0200

    images

--- portfolio_builder/urls.py
+++ portfolio_builder/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -31,4 +33,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0)),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
