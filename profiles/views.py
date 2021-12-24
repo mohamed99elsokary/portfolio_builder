77ff87ebfce4b1080f0faa7fc22adaa4d51e5073
mohamed <80061689+mohamed99elsokary@users.noreply.github.com>
2021-12-25 00:09:05 +0200

    Update views.py

--- profiles/views.py
+++ profiles/views.py
@@ -10,10 +10,11 @@ from work_experience import models as work_experience_models
 def data(domain):
     domain = domain.split(".")[0]
     print(domain)
+    account = models.profiles.objects.get(subject=domain)
     # token = "59c55426af71d2f51b374fa4704d5ac35633bfa9"
-    token = "4f9fa19ae82e8253e633d1843f40e35997f2b9c2"
-    key = Token.objects.get(key=token)
-    user = models.profiles.objects.get(user=key.user)
+    # token = "4f9fa19ae82e8253e633d1843f40e35997f2b9c2"
+    # key = Token.objects.get(key=token)
+    user = account.user
     languages = models.Languages.objects.filter(profile_id=user)
     contact_info = models.contact_info.objects.filter(profile_id=user)
     skills = skills_models.user_skills.objects.filter(profile_id=user)
