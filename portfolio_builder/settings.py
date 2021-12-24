d16e0768ab7742e57b07bacfb62e6e2bffeefabf
mohamed <80061689+mohamed99elsokary@users.noreply.github.com>
2021-12-24 21:34:39 +0200

    Update settings.py

--- portfolio_builder/settings.py
+++ portfolio_builder/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 #  -------------------linux----------------#
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 # -------------------linux----------------#
