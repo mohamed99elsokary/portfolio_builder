f76f721a7d16abb67088dc7bfba0eb5a1bb4bf39
mohamed <80061689+mohamed99elsokary@users.noreply.github.com>
2021-12-24 21:21:51 +0200

    Update settings.py

--- portfolio_builder/settings.py
+++ portfolio_builder/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR,
     "static",
